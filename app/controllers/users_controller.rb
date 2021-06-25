0b7dd894b95c3840ac3dcf995bbce7742619feae
yhweb-star <yh48121229@gmail.com>
2021-06-25 12:06:11 +0900

    users/show関連ページの部分テンプレート作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,43 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :tweet, :meal, :training, :success]
+
   def show
-    @user = User.find(params[:id])
-    @user_area = Area.find(@user.chara.area_id)
-    @user_gender = Gender.find(@user.chara.area_id)
     @tweets = @user.tweets
     @meals = @user.meals
     @trainings = @user.trainings
     @successes = @user.successes
-
-    #@instances = (@tweets + @meals + @trainings + @successes).sort_by {|record| record.created_at}.reverse!
 
     @instances = @tweets | @meals | @trainings | @successes
     @instances.sort!{ |a, b| b.created_at <=> a.created_at }
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
   end
 
   def tweet
-    @user = User.find(params[:id])
-    @user_area = Area.find(@user.chara.area_id)
-    @user_gender = Gender.find(@user.chara.area_id)
     @tweets = @user.tweets
   end
 
   def meal
-    @user = User.find(params[:id])
-    @user_area = Area.find(@user.chara.area_id)
-    @user_gender = Gender.find(@user.chara.area_id)
     @meals = @user.meals
   end
 
   def training
-    @user = User.find(params[:id])
-    @user_area = Area.find(@user.chara.area_id)
-    @user_gender = Gender.find(@user.chara.area_id)
     @trainings = @user.trainings
   end
 
   def success
+    @successes = @user.successes
+  end
+
+  private
+  def set_user
     @user = User.find(params[:id])
     @user_area = Area.find(@user.chara.area_id)
     @user_gender = Gender.find(@user.chara.area_id)
-    @successes = @user.successes
   end
 
 end
